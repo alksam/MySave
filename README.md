46e95cf8d6fa8e1d345f77822ae021db3f3771f6
alksam <118382595+alksam@users.noreply.github.com>
2024-04-02 10:42:07 +0200

    new2foto

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Domain digram
 
-![img.png](img.png)
+![img_2.png](img_2.png)
 
 ![img_1.png](img_1.png)
---
+++ src/main/java/app/model/Category.java
@@ -0,0 +1,23 @@
+package app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "categories")
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false, unique = true)
+    private int CategoryID;
+    @Column(name = "category_name")
+    private String CategoryName;
+
+}
