ec8e1c2a2cb93c34065ba80dd0fdce14430d2ab5
MGold <mikkelgold@protonmail.com>
2024-04-02 10:47:09 +0200

    updated event

--- src/main/java/app/model/Event.java
+++ src/main/java/app/model/Event.java
@@ -1,2 +1,21 @@
 package app.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "EventId", nullable = false, unique = true)
+    private int EventId;
+}
